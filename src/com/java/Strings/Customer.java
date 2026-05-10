e4f01ee69c831d8e993931e7975fc34f1333711a
Mukkera Hariprasad <mukkerahariprasad1849@gmail.com>
2026-05-10 16:57:29 +0530

    Added coding questions programs (even,odd,reverse numbers) Added sum of N numbers program Implemented multiplication table logic Implemented logic to extract and print digits using modulo and division Add reverse number and sum of digits programs Implemented logic to calculate product of digits using while loop Add Java program to calculate product of digits Added palindrome number check using reverse logic Implemented Armstrong number check using power logic Added Java program to find the maximum digit in a number using loop and modulo operation Added program to find minimum digit in a number Implement Java program to count even and odd digits in a number using while loop Add optimized Java program to check prime number using square root approach Added program to print prime numbers from 1 to N Add factorial program in Java Add Java program to find factors of a number Solved array coding questions and implemented Product array object examples Learned Java string concepts with constructors, objects, string methods, regex, arrays, and type conversion

---
+++ src/com/java/Strings/Customer.java
@@ -0,0 +1,9 @@
+package com.java.Strings;
+
+public class Customer {
+    int id;
+    String name;
+    String email;
+    String phone;
+
+}
---
+++ src/com/java/Strings/StringTest.java
@@ -0,0 +1,75 @@
+package com.java.Strings;
+
+public class StringTest {
+    StringTest(){
+        System.out.println("My String constructor");
+
+    }
+    public static void main (String[] args){
+        StringTest str = new StringTest();
+       /* Product product = new Product();
+        System.out.println(product);*/
+        System.out.println(str);
+
+        String str1 = new String();
+        System.out.println("Str1:" + str1);
+        char[] chars = { 'a','b','c'};
+        String str2 = new String(chars);
+        System.out.println("str2:" + str2);
+        String str3 = new String("abc");
+        System.out.println("str3:" + str3);
+        String str4 = str2;
+        System.out.println("str4:" + str4);
+        System.out.println(str2 == str4);
+        System.out.println(str2 == str3);
+
+        String str5 = "abc";
+        String str6 = "abc";
+        System.out.println(str5 == str6);
+
+        System.out.println(chars.length);
+        System.out.println(str5.length());
+        System.out.println(str5.charAt(1));
+        System.out.println(str5.compareToIgnoreCase("aBc"));
+        String name = "john";
+
+        name = name.concat(",how are you?");
+        System.out.println(name);
+        String username = "abc:xyz";
+        String pasword = "abcd@123";
+        if (username.isEmpty() || pasword.isEmpty()) {
+            System.out.println("Enter the details");
+
+        }
+        if (username.equalsIgnoreCase("MaDhu") && pasword.equals("abc@123"))
+            System.out.println("login successful");
+        else
+            System.out.println("login failed ! Invalid Credentials");
+        String usernamePattern = "^[a-zA-Z0-9_]{4,15}$";
+            System.out.println(username.matches(usernamePattern));
+
+        String customerData = "1,JohnDoe,johndoe@gmail.com,123-456-789";
+        String[] data = customerData.split(",");
+        Customer customer = new Customer();
+
+        customer.id = Integer.parseInt(data[0]);
+        customer.name = data[1];
+        customer.email = data[2];
+        customer.phone = data[3];
+        System.out.println("customer Id:" + customer.id);
+        System.out.println("name:" + customer.name);
+        System.out.println("email:" + customer.email);
+        System.out.println("phone:" + customer.phone);
+
+        String password1 = "       Abcd @123     ";
+        System.out.println(password1.length());
+        System.out.println(password1.trim().length());
+        int phoneNo = 324234;
+        System.out.println(String.valueOf(phoneNo));
+
+
+
+
+
+    }
+}
