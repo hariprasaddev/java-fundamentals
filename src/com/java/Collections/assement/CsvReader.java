c05f17dfe631accd594e13ef788079b439dd8764
Hariprasad18 <mukkerahariprasad1849@gmail.com>
2026-06-10 21:05:05 +0530

    initial commit

---
+++ src/com/java/Collections/assement/CsvReader.java
@@ -0,0 +1,61 @@
+package com.java.Collections.assement;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//public class CsvReader {
+    /*
+    private List<Product> products;
+    private CsvReader csvReader;
+
+    public ProductService(CsvReader csvReader){
+        this.csvReader = csvReader;
+        products = this.csvReader.getProductsFromCsv();
+    }
+
+    /*
+    CRUD
+    Create -> new Product
+    REad -> existing product read
+        -> existing all products
+    Update -> existing product update
+    Delete -> existing product delete
+     */
+
+    /*public List<Product> getAllProducts(){
+        return products;
+    }
+    public Product addProduct(Product product){
+        products.add(product);
+        return product;
+    }
+    public Product getProductById(int id){
+        Product getProductById = null;
+        for(Product product : products){
+            if(product.getId() == id){
+                getProductById = product;
+            }
+        }
+        return getProductById;
+    }
+
+    public Product updateProduct(Product product){
+        int index = 0;
+        for(Product p : products){
+            if(p.getId() == product.getId()){
+                products.set(index, product);
+                return product;
+            }
+            index++;
+        }
+        return null;
+
+    }
+
+
+}
+}*/
